c2209c6d995603be1f15f021aabae13c40fedda7
kooros <37519785+kooros@users.noreply.github.com>
2026-04-27 12:44:17 +0200

    Add files via upload
    Final Version

---
+++ Pre_Filter_PS.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Directory | ForEach-Object {
+    $files = Get-ChildItem $_.FullName -File -Recurse -ErrorAction SilentlyContinue
+    [PSCustomObject]@{
+        Folder     = $_.Name
+        FileCount  = $files.Count
+        TotalSizeMB = [math]::Round(($files | Measure-Object Length -Sum).Sum / 1MB, 2)
+    }
+} | Sort-Object Folder | Export-Csv "FolderReport.csv" -NoTypeInformation
